2975a95ab9e8b9724dd5a9df6460e58acf7d52fe
Oleg Lavrovsky <oleg@utou.ch>
2015-06-28 12:02:47 +0200

    Form validation and presentation

--- app.js
+++ app.js
@@ -150,6 +150,23 @@ template._onThreadTap = function(e) {
   this.$.threadlist.select(idx);
 };
 
+template.openDialog = function(e) {
+  var button = e.target;
+  while (!button.hasAttribute('data-dialog') && button !== document.body) {
+    button = button.parentElement;
+  }
+  if (!button.hasAttribute('data-dialog')) {
+    return;
+  }
+
+  var id = button.getAttribute('data-dialog');
+  var dialog = document.getElementById(id);
+
+  if (dialog) {
+    dialog.open();
+  }
+};
+
 template.toggleSearch = function() {
   this.$.search.toggle();
 };
